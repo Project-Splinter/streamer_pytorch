8f8f30549eed26ccbf59c764615c01351dcdbd48
Ruilong Li <397653553@qq.com>
2020-04-22 21:30:07 -0700

    init

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+INSTALL_REQUIREMENTS = ['numpy', 'torch', 'opencv-python', 'tqdm', 'imageio']
+
+setuptools.setup(
+    name='streamer_pytorch',
+    url='https://github.com/liruilong940607/streamer_pytorch',
+    description='Pytorch based data streamer. (Capture, Video & Image).',    
+    version='0.0.1',
+    author='Ruilong Li',
+    author_email='ruilongl@usc.edu',    
+    license='MIT License',
+    packages=setuptools.find_packages(),
+    install_requires=INSTALL_REQUIREMENTS,
+)
+
---
+++ streamer_pytorch/__init__.py
@@ -0,0 +1,4 @@
+from streamer import BaseStreamer
+from streamer import CaptureStreamer
+from streamer import VideoListStreamer
+from streamer import ImageListStreamer
